e74d5f6a1654606dbfac666723de18034046d1c2
JuanHenao <judhenaosa@unal.edu.co>
2016-04-23 22:50:23 -0500

    function

--- ComNet.R
+++ ComNet.R
@@ -38,15 +38,17 @@ CommonModules <- function(G1,G2,method){
     for(j in 1:length(graph2)){
       subB <- induced.subgraph(smallGraph,vids = as.vector(unlist(graph2[j])))
       
-      if(length(names(subA[2])) > length(names(subB[2]))){
-        dif <- graph.difference(subA,subB)
-      }else if(length(names(subA[2])) < length(names(subB[2]))){
-        dif <- graph.difference(subB,subA)
-      }else{
-        dif <- graph.difference(subA,subB)
-        if(names(subA[2]) == names(subB[2]) && ecount(dif) == 0){
-          iqual[[counter]] <- names(dif[2])
-          counter <- counter + 1
+      if(ecount(subA) > 0 && ecount(subB) > 0){
+        if(length(names(subA[2])) > length(names(subB[2]))){
+          dif <- graph.difference(subA,subB)
+        }else if(length(names(subA[2])) < length(names(subB[2]))){
+          dif <- graph.difference(subB,subA)
+        }else{
+          dif <- graph.difference(subA,subB)
+          if(names(subA[2]) == names(subB[2]) && ecount(dif) == 0){
+            iqual[[counter]] <- names(dif[2])
+            counter <- counter + 1
+          }
         }
       }
     }
@@ -68,21 +70,22 @@ AvsM2 <- CommonModules(ADN,MSN,method = "walk")
 
 ####################################################
 
-method = "walk"
+method = "fgr"
 
 if(method == "fgr"){
-  MoA <- cluster_fast_greedy(G1)
-  MoB <- cluster_fast_greedy(G2)
+  MoA <- cluster_fast_greedy(ADN)
+  MoB <- cluster_fast_greedy(PDN)
 }else if(method == "ebet"){
   MoA <- cluster_edge_betweenness(G1,directed = F)
   MoB <- cluster_edge_betweenness(G2,directed = F)
 }else if(method == "leig"){
-  MoA <- cluster_leading_eigen(G1)
-  MoB <- cluster_leading_eigen(G2)
+  MoA <- cluster_leading_eigen(ADN)
+  MoB <- cluster_leading_eigen(PDN)
 }else if(method == "walk"){
   MoA <- cluster_walktrap(G1)
   MoB <- cluster_walktrap(G2)
 }
+
 
 if(length(MoA) >= length(MoB)){
   graph1 <- MoA
@@ -98,25 +101,24 @@ if(length(MoA) >= length(MoB)){
 
 iqual <- vector(mode = 'list')
 counter <- 1
-  
+
 for(i in 1:length(graph1)){
   subA <- induced.subgraph(bigGraph,vids = as.vector(unlist(graph1[i])))
   
   for(j in 1:length(graph2)){
     subB <- induced.subgraph(smallGraph,vids = as.vector(unlist(graph2[j])))
     
-    if(length(names(subA[2])) > length(names(subB[2]))){
-      print("subA1 > subP1")
-      dif <- graph.difference(subA,subB)
-    }else if(length(names(subA[2])) < length(names(subB[2]))){
-      print("subP1 > subA1")
-      dif <- graph.difference(subB,subA)
-    }else{
-      print("subA1 = subP1")
-      dif <- graph.difference(subA,subB)
-      if(names(subA[2]) == names(subB[2]) && ecount(dif) == 0){
-        iqual[[counter]] <- names(dif[2])
-        counter <- counter + 1
+    if(ecount(subA) > 0 && ecount(subB) > 0){
+      if(length(names(subA[2])) > length(names(subB[2]))){
+        dif <- graph.difference(subA,subB)
+      }else if(length(names(subA[2])) < length(names(subB[2]))){
+        dif <- graph.difference(subB,subA)
+      }else{
+        dif <- graph.difference(subA,subB)
+        if(names(subA[2]) == names(subB[2]) && ecount(dif) == 0){
+          iqual[[counter]] <- names(dif[2])
+          counter <- counter + 1
+        }
       }
     }
   }
