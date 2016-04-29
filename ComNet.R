055a9be6535f8f726c91066e81acd4e4a01681b5
JuanHenao <judhenaosa@unal.edu.co>
2016-04-29 10:13:44 -0500

    corrections

--- ComNet.R
+++ ComNet.R
@@ -164,9 +164,9 @@ SubC <- function(Ma,Md,Me){
   }
 }
 
-AD <- cluster_fast_greedy(ADN)
-PD <- cluster_fast_greedy(PDN)
-MS <- cluster_fast_greedy(MSN)
+AD <- cluster_walktrap(ADN)
+PD <- cluster_walktrap(PDN)
+MS <- cluster_walktrap(MSN)
 
 for(i in 1:length(AD)){
   for(j in 1:length(PD)){
@@ -175,73 +175,43 @@ for(i in 1:length(AD)){
          length(as.vector(unlist(AD[i]))) < length(as.vector(unlist(MS[k])))) &&
          length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(PD[j])))){
         t <- SubC(PD[j],MS[k],AD[i])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-           ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }else if((length(as.vector(unlist(AD[i]))) < length(as.vector(unlist(PD[j]))) &&
                 length(as.vector(unlist(AD[i]))) < length(as.vector(unlist(MS[k])))) &&
                length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(MS[k])))){
         t <- SubC(MS[k],PD[j],AD[i])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-        ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }else if((length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(AD[i]))) &&
                 length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(MS[k])))) &&
                length(as.vector(unlist(AD[i]))) < length(as.vector(unlist(MS[k])))){
         t <- SubC(MS[k],AD[i],PD[j])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-        ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }else if((length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(AD[i]))) &&
                 length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(MS[k])))) &&
                length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(AD[i])))){
         t <- SubC(AD[i],MS[k],PD[j])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-        ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }else if((length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(AD[i]))) &&
                 length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(PD[j])))) &&
                length(as.vector(unlist(PD[j]))) < length(as.vector(unlist(AD[i])))){
         t <- SubC(AD[i],PD[j],MS[k])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-        ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }else if((length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(AD[i]))) &&
                 length(as.vector(unlist(MS[k]))) < length(as.vector(unlist(PD[j])))) &&
                length(as.vector(unlist(AD[i]))) < length(as.vector(unlist(PD[j])))){
         t <- SubC(PD[j],AD[i],MS[k])
-        if(is.null(t) != TRUE && length(as.vector(unlist(AD[i]))) <= 10 &&
-           length(as.vector(unlist(PD[j]))) <=10 && length(as.vector(unlist(MS[k]))) <= 10
-        ){
-          a <- AD[i]
-          b <- PD[j]
-          c <- MS[k]
-          stop("break")
+        if(is.null(t) != TRUE){
+          stop("aparecio")
         }
       }
     }
